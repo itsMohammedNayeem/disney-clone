3f5432c9b26e6101d6698a15f521fbe23273a0a4
“itsMohammedNayeem” <Mohammed.Nayeem@weareplanet.com>
2023-12-20 23:57:12 +0530

    Fix API call and error handling in getAISuggestion.ts

--- function/src/functions/getAISuggestion.ts
+++ function/src/functions/getAISuggestion.ts
@@ -18,21 +18,30 @@ export async function getAISuggestion(
   context.log(`Http function processed request for url "${request.url}"`);
   const term = request.query.get("term");
 
-  const completion = await openai.chat.completions.create({
-    messages: [
-      {
-        role: "system",
-        content: `You are a digital video assistant working for services such as Netflix, Disney Plus & Amazon Prime Video. Your job is to provide suggestions based on the videos the user specifies. Provide an quirky breakdown of what the user should watch next! It should only list the names of the films after the introduction. Keep the response short and sweet! Always list at least 3 films as suggestions. If the user mentions a genre, you should provide a suggestion based on that genre.`,
-      },
-      {
-        role: "user",
-        content: `I like: ${term}`,
-      },
-    ],
-    model: "gpt-3.5-turbo",
-  });
+    try {
+      const completion = await openai.chat.completions.create({
+        messages: [
+          {
+            role: "system",
+            content: `You are a digital video assistant working for services such as Netflix, Disney Plus & Amazon Prime Video. Your job is to provide suggestions based on the videos the user specifies. Provide an quirky breakdown of what the user should watch next! It should only list the names of the films after the introduction. Keep the response short and sweet! Always list at least 3 films as suggestions. If the user mentions a genre, you should provide a suggestion based on that genre.`,
+          },
+          {
+            role: "user",
+            content: `I like: ${term}`,
+          },
+        ],
+        model: "gpt-3.5-turbo",
+      });
 
-  return { body: completion.choices[0].message.content || "No suggestion" };
+      return {
+        body: completion.choices[0].message.content ?? "No suggestion",
+      };
+    } catch (err) {
+      console.error("🚀 ~ file: getAISuggestion.ts:40 ~ err:", err);
+      context.log("🚀 ~ file: getAISuggestion.ts:40 ~ err:", err);
+      return { body: "No suggestion" };
+    }
+
 }
 
 app.http("getAISuggestion", {
